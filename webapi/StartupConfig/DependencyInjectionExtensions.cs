40437ccbf4c88845fe559a7542e71233ec849e4a
livingmartialway <livingmartialway@gmail.com>
2023-06-08 14:31:56 -0500

    Add project files.

---
+++ webapi/StartupConfig/DependencyInjectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using IntranetLibrary.DataAccess;
+
+namespace IntranetAPI.StartupConfig;
+
+public static class DependencyInjectionExtensions
+{
+    public static void AddServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+        builder.Services.AddSingleton<IReportTestData, ReportTestData>();
+        builder.Services.AddHealthChecks()
+            .AddSqlServer(builder.Configuration.GetConnectionString("Default"));
+    }
+
+
+
+}
